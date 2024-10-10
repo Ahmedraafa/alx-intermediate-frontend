7cce7200748c2525c4955f084b9908c31f654e54
Ahmed Raafat <122217903+Ahmedraafa@users.noreply.github.com>
2024-10-10 16:53:53 +0300

    Resolve merge conflict between main and feature-greeting

---
+++ Downloads/Weather-Application-main/Weather-Application-main/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // تأكد من أن هذا هو المسار الصحيح
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
